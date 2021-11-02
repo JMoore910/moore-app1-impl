9e9c280e31fd38d9ffb0e20c3149837ea6da9acb
Jeanne Moore <mooresea910@gmail.com>
2021-11-01 21:54:21 -0400

    Check to see if implementation app runs and build file

--- src/main/java/baseline/ToDoListApplication.java
+++ src/main/java/baseline/ToDoListApplication.java
@@ -5,7 +5,25 @@ package baseline;
  *  Copyright 2021 Jeanne Moore
  */
 
+import java.util.Objects;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class ToDoListApplication {
+public class ToDoListApplication extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ToDoApp.fxml")));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("To Do List");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
